d43bdc4584015ce5bc5efb2a2d36b33474bb2256
Enes Simsek <y.enes_simsek@hotmail.com>
2024-04-30 16:39:53 +0300

    second commit

--- hangman.py
+++ hangman.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 name = input("enter your name: ")
 print(f"Hi {name}!, Welcome to Hangman Game!, lets start to play.")
-
+#########################
 secret_word = "Metallica"
 guess_string = ""
 lives = 10
